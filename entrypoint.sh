06a2c7be35d00972cc90ea0404f3984ea20beea0
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-10-19 12:09:40 +0100

    Run paleoplay command from root dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running Paleoplay for ${INPUT_USER}"
-paleoplay post --user ${INPUT_USER} --tinify-api-key ${INPUT_TINIFY_API_KEY}
+/paleoplay post --user ${INPUT_USER} --tinify-api-key ${INPUT_TINIFY_API_KEY}
